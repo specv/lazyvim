a0e4747f3008ff81342d62b22ca81740a0c0c314
specv <61646426+specv@users.noreply.github.com>
2024-06-13 17:35:30 +0800

    Set terminal title

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- set the terminal title, so that we can identify lazyvim in `yabai -m query --windows --window`
+vim.opt.title = true
+vim.opt.titlestring = "LazyVim"
