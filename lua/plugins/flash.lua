5c1a1033821728eaa9443ca991369af6843358dd
specv <61646426+specv@users.noreply.github.com>
2024-06-13 16:55:24 +0800

    flash: label color

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/flash.nvim",
+  opts = function()
+    vim.api.nvim_set_hl(0, "FlashLabel", { bg = "#ff007c", fg = "#c8d3f5", bold = true })
+    vim.api.nvim_set_hl(0, "FlashMatch", { bg = "#2e7de9", fg = "#c8d3f5", bold = true })
+    vim.api.nvim_set_hl(0, "FlashCurrent", { bg = "#ff966c", fg = "#ffffff", bold = true })
+    vim.api.nvim_set_hl(0, "FlashBackdrop", { fg = "#545c7e", bold = true })
+  end
+}
