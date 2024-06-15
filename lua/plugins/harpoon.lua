4b05e97aa447d86232fe02f9f69412ff9fdad25b
specv <61646426+specv@users.noreply.github.com>
2024-06-15 22:11:42 +0800

    harpoon.nvim

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,14 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  keys = {
+    { "<leader>H", false },
+    {
+      "<leader>0",
+      function()
+        require("harpoon"):list():add()
+      end,
+      desc = "Harpoon File",
+    },
+  }
+}
