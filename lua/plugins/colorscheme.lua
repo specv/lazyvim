54197568c0b82e17c7d73a1f5e2fe5d55d5f4ffe
specv <61646426+specv@users.noreply.github.com>
2024-06-15 22:13:53 +0800

    gruvbox-material.nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,32 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
-
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_background = "soft"
+      vim.opt.background = "dark"
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+    keys = {
+      {
+        "<leader>wt",
+        function()
+          if vim.o.background == "light" then
+            vim.o.background = "dark"
+          else
+            vim.o.background = "light"
+          end
+        end,
+        desc = "Toggle Dark and Light Theme",
+      },
+    }
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "gruvbox-material",
     },
   }
 }
