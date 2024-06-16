caae16b8fdf9275d6212fb4010383d6522d5881c
specv <61646426+specv@users.noreply.github.com>
2024-06-16 12:11:38 +0800

    unfocused-cursor.nvim

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "levouh/tint.nvim", enabled = false,
+  },
+}
---
+++ lua/plugins/unfocused-cursor.lua
@@ -0,0 +1,6 @@
+return {
+  "amarakon/nvim-unfocused-cursor",
+  config = function()
+    require("unfocused-cursor").setup()
+  end,
+}
