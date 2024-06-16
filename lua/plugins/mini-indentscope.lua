d42c73f3a6c24acb27ce01c0eef1560ddd7ee94c
specv <61646426+specv@users.noreply.github.com>
2024-06-16 21:09:37 +0800

    Upgrade yazi.nvim

--- lua/plugins/mini-indentscope.lua
+++ lua/plugins/mini-indentscope.lua
@@ -2,5 +2,15 @@ return {
   "echasnovski/mini.indentscope",
   opts = {
     symbol = '╎',
-  }
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "yazi",
+      },
+      callback = function()
+        vim.b.miniindentscope_disable = true
+      end
+    })
+  end
 }
--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,10 +1,26 @@
 return {
-  "DreamMaoMao/yazi.nvim",
+  "mikavilpas/yazi.nvim",
   dependencies = {
-    "nvim-telescope/telescope.nvim",
     "nvim-lua/plenary.nvim",
   },
+  event = "VeryLazy",
   keys = {
-    { "<leader>fy", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
+    {
+      "<leader>fy",
+      function()
+        require("yazi").yazi()
+      end,
+      desc = "Open the Yazi",
+    },
+    {
+      "<leader>fY",
+      function()
+        require("yazi").yazi(nil, vim.fn.getcwd())
+      end,
+      desc = "Open the Yazi in working directory" ,
+    },
+  },
+  opts = {
+      open_for_directories = false,
   },
 }
