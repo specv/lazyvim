e12eb8e273a4e3543f27848c891f0165d4d27f91
specv <61646426+specv@users.noreply.github.com>
2024-06-25 06:36:50 +0800

    chatgpt.nvim

---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,17 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup {
+      openai_params = {
+        model = "gpt-4-32k-0613",
+      }
+    }
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim"
+  }
+}
