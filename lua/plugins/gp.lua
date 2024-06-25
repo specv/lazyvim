9b72ca51e66ce2576a996b4cd3c69acde6807018
specv <61646426+specv@users.noreply.github.com>
2024-06-25 16:40:23 +0800

    gp.nvim: switch agent by telescope

--- lua/plugins/gp.lua
+++ lua/plugins/gp.lua
@@ -1,16 +1,72 @@
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
+local conf = require('telescope.config').values
+
+local function get_relevant_agents()
+  local gp = require("gp")
+  local agents = {}
+  local buf = vim.api.nvim_get_current_buf()
+  local file_name = vim.api.nvim_buf_get_name(buf)
+  local is_chat = gp.not_chat(buf, file_name) == nil
+  local current_agent, agent_list
+
+  if is_chat then
+    current_agent = gp._state.chat_agent
+    agent_list = gp._chat_agents
+  else
+    current_agent = gp._state.command_agent
+    agent_list = gp._command_agents
+  end
+
+  for _, name in ipairs(agent_list) do
+    if name == current_agent then
+      table.insert(agents, "* " .. name)
+    else
+      table.insert(agents, "  " .. name)
+    end
+  end
+
+  return agents, is_chat
+end
+
+local function select_agent()
+  local gp = require("gp")
+  local agents, is_chat = get_relevant_agents()
+  local prompt_title = is_chat and 'Select Chat Agent' or 'Select Command Agent'
+
+  pickers.new({}, {
+    prompt_title = prompt_title,
+    finder = finders.new_table {
+      results = agents
+    },
+    sorter = conf.generic_sorter({}),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        local agent_name = selection[1]:sub(3)
+        gp.cmd.Agent({ args = agent_name })
+      end)
+      return true
+    end,
+  }):find()
+end
+
 return {
   "robitx/gp.nvim",
   lazy = false,
   keys = {
     { "<leader>i", "", desc = "gpt", mode = { "n", "v" } },
-    { "<leader>if", "<cmd>GpChatFinder<cr>", desc = "Chat Finder" },
+    { "<leader>ie", "<cmd>GpChatFinder<cr>", desc = "Chat Explorer" },
     { "<leader>ii", "<cmd>GpChatToggle<cr>", desc = "Chat Toggle", mode = "n" },
     { "<leader>ii", ":<C-u>'<,'>GpChatPaste<cr>", desc = "Chat Toggle", mode = "v" },
     { "<leader>ir", "<cmd>GpAppend<cr>", desc = "Rewrite", mode = "n" },
     { "<leader>ir", ":<C-u>'<,'>GpAppend<cr>", desc = "Rewrite", mode = "v" },
     { "<leader>ic", "<cmd>GpContext<cr>", desc = "Modify Context" },
     { "<leader>is", "<cmd>GpStop<cr>", desc = "Stop Generating" },
-    { "<leader>ia", pick_agent, desc = "Pick Agent" },
+    { "<leader>ia", select_agent, desc = "Select Agent" },
   },
   config = function()
     local gp = require("gp")
