cfb51beca48b41766e8742e00c8f9cbb66f05f43
specv <61646426+specv@users.noreply.github.com>
2024-06-30 21:54:49 +0800

    Switch to `markdownlint-cli2` https://github.com/LazyVim/LazyVim/commit/f47d105b80dce615a572a13351a137653444d548

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -3,7 +3,7 @@ return {
   event = "LazyFile",
   opts = {
     linters = {
-      markdownlint = {
+      ["markdownlint-cli2"] = {
         -- disable markdown lint in gp.nvim
         condition = function(ctx)
           return not (string.find(ctx.filename, "/gp/chats") and string.match(ctx.filename, "%.md$"))
