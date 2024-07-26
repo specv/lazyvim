53f806149e5ef555bb455dfc28b694e6bad14db6
specv <61646426+specv@users.noreply.github.com>
2024-07-26 21:04:22 +0800

    Add more language servers to `mason.lua`

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,7 +5,21 @@ return {
   },
   opts = {
     ensure_installed = {
+      "elixir-ls",
+      "pyright",
+      "ruff",
+      "ruff-lsp",
+      "stylua",
+      "lua-language-server",
+      "typescript-language-server",
       "svelte-language-server",
+      "marksman",
+      "markdown-toc",
+      "markdownlint-cli2",
+      "alex",
+      "shfmt",
+      "json-lsp",
+      "taplo",
     }
   },
 }
